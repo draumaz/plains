577dc984078fd051df986fee35dbf9b688585c3e
draumaz <77967471+draumaz@users.noreply.github.com>
2021-02-08 20:47:21 -0800

    Finished third tree, added variable scan feature

--- main.c
+++ main.c
@@ -53,7 +53,7 @@ SUB1:
 	memset(nav1, '\0', 50);
 	scanf("%s", nav1);
 
-		if(strcmp(nav1, "1")==0){
+	if(strcmp(nav1, "1")==0){
 		system("clear");
 		printf("\nThe Plains v0.08\n\n");
 		memset(nav1, '\0', 50);
@@ -62,36 +62,34 @@ SUB1:
 		printf("[B]ACK\n\n");
 		printf("\n\n\nACTION >> ");
 		scanf("%s", nav1);
-			if(strcmp(nav1, "E")==0){
-			printf("\nIn a moment of mind-blowing wit, you willingly let yourself die.\nNice going, genius.\n\n");
-			printf("GAME OVER\n\n");
-			sleep(5);
-			return 0;
-			}
-	
-			if(strcmp(nav1, "e")==0){
-			printf("\nIn a moment of mind-blowing wit, you willingly let yourself die.\nNice going, genius.\n\n");
-			printf("GAME OVER\n\n");
-			sleep(5);
-			return 0;
-			}
-			if(strcmp(nav1, "B")==0){
-			printf("\nYou decide to head back to where you started.\nProbably for the best.\n");
-			sleep(2);
-			goto SUB1;
-			}
-			if(strcmp(nav1, "b")==0){
-			printf("\nYou decide to head back to where you started.\nProbably for the best.\n");
-			sleep(2);
-			goto SUB1;
-			}
-	
-			else {
-			goto SUB1;
-			}
+		if(strcmp(nav1, "E")==0){
+		printf("\nIn a moment of mind-blowing wit, you willingly let yourself die.\nNice going, genius.\n\n");
+		printf("GAME OVER\n\n");
+		sleep(5);
+		return 0;
 		}
+		if(strcmp(nav1, "e")==0){
+		printf("\nIn a moment of mind-blowing wit, you willingly let yourself die.\nNice going, genius.\n\n");
+		printf("GAME OVER\n\n");
+		sleep(5);
+		return 0;
+		}
+		if(strcmp(nav1, "B")==0){
+		printf("\nYou decide to head back to where you started.\nProbably for the best.\n");
+		sleep(2);
+		goto SUB1;
+		}
+		if(strcmp(nav1, "b")==0){
+		printf("\nYou decide to head back to where you started.\nProbably for the best.\n");
+		sleep(2);
+		goto SUB1;
+		}
+		else {
+		goto SUB1;
+		}
+	}
 
-		if(strcmp(nav1, "2")==0){
+	if(strcmp(nav1, "2")==0){
 		system("clear");
 		printf("\nThe Plains v0.08\n\n");
 		memset(nav1, '\0', 50);
@@ -100,6 +98,32 @@ SUB1:
 		printf("[B]ACK\n");
 		printf("\n\n\n\n\nACTION >> ");
 		scanf("%s", nav1);
+		if(strcmp(nav1, "B")==0){
+		printf("\nYou stop being motionless and return to a life full of motion.\n");
+		sleep(2);
+		goto SUB1;
+		}
+		if(strcmp(nav1, "E")==0){
+		printf("\nYou continue standing there.\n");
+		sleep(10);
+		printf("Seriously, why are you doing this?\n");
+		printf("\nK[E]EP STANDING");
+		printf("\n[B]ACK\n");
+		printf("\nACTION >> ");
+		memset(nav1, '\0', 50);
+		scanf("%s", nav1);
+			if(strcmp(nav1, "E")==0){
+			sleep(15);
+			printf("\nYou've been standing there for a while. Shouldn't you head back?\n");
+			sleep(4);
+			goto SUB1;
+			}
+			if(strcmp(nav1, "e")==0){
+			sleep(15);
+			printf("\nYou've been standing there for a while. Shouldn't you head back?\n");
+			sleep(4);
+			goto SUB1;
+			}
 			if(strcmp(nav1, "B")==0){
 			printf("\nYou stop being motionless and return to a life full of motion.\n");
 			sleep(2);
@@ -110,40 +134,9 @@ SUB1:
 			sleep(2);
 			goto SUB1;
 			}
-			if(strcmp(nav1, "E")==0){
-			printf("\nYou continue standing there.\n");
-			sleep(10);
-			printf("Seriously, why are you doing this?\n");
-			printf("\nK[E]EP STANDING");
-			printf("\n[B]ACK\n");
-			printf("\nACTION >> ");
-			memset(nav1, '\0', 50);
-			scanf("%s", nav1);
-				if(strcmp(nav1, "E")==0){
-				sleep(5);
-				printf("\nStanding there completely motionlees, it gives you the feeling that everything's going to be alright.\n");
-				sleep(4);
-				goto SUB1;
-				}
-				if(strcmp(nav1, "e")==0){
-				sleep(5);
-				printf("\nStanding there completely motionless, it gives you the feeling that eberything's going to be alright.\n");
-				sleep(4);
-				goto SUB1;
-				}
-				if(strcmp(nav1, "B")==0){
-				printf("\nYou stop being motionless and return to a life full of motion.\n");
-				sleep(2);
-				goto SUB1;
-				}
-				if(strcmp(nav1, "b")==0){
-				printf("\nYou stop being motionless and return to a life full of motion.\n");
-				sleep(2);
-				goto SUB1;
-				}
-				else {
-				goto SUB1;
-				}}
+			else {
+			goto SUB1;
+			}}
 			if(strcmp(nav1, "e")==0){
 			printf("\nYou continue standing there.\n");
 			sleep(10);
@@ -153,31 +146,31 @@ SUB1:
 			printf("\nACTION >> ");
 			memset(nav1, '\0', 50);
 			scanf("%s", nav1);
-				if(strcmp(nav1, "E")==0){
-				sleep(5);
-				printf("\nStanding there completely motionless, it gives you the feeling that everything's going to be alright.\n");
-				sleep(4);
-				goto SUB1;
-				}
-				if(strcmp(nav1, "e")==0){
-				sleep(5);
-				printf("\nStanding there completely motionless, it gives you the feeling that everything's going to be alright.\n");
-				sleep(4);
-				goto SUB1;
-				}
-				if(strcmp(nav1, "B")==0){
-				printf("\nYou stop being motionless and return to a life full of motion.\n");
-				sleep(2);
-				goto SUB1;
-				}
-				if(strcmp(nav1, "b")==0){
-				printf("\nYou stop being motionless and return to a life full of motion.\n");
-				sleep(2);
-				goto SUB1;
-				}}
+			if(strcmp(nav1, "E")==0){
+			sleep(5);
+			printf("\nStanding there completely motionless, it gives you the feeling that everything's going to be alright.\n");
+			sleep(4);
+			goto SUB1;
 			}
+			if(strcmp(nav1, "e")==0){
+			sleep(5);
+			printf("\nStanding there completely motionless, it gives you the feeling that everything's going to be alright.\n");
+			sleep(4);
+			goto SUB1;
+			}
+			if(strcmp(nav1, "B")==0){
+			printf("\nYou stop being motionless and return to a life full of motion.\n");
+			sleep(2);
+			goto SUB1;
+			}
+			if(strcmp(nav1, "b")==0){
+			printf("\nYou stop being motionless and return to a life full of motion.\n");
+			sleep(2);
+			goto SUB1;
+			}}
+	}
 	
-		if(strcmp(nav1, "3")==0){
+	if(strcmp(nav1, "3")==0){
 		system("clear");
 		printf("\nThe Plains v0.08\n\n");
 		memset(nav1, '\0', 50);
@@ -186,38 +179,38 @@ SUB1:
 		printf("[B]ACK\n\n");
 		printf("\n\n\nACTION >> ");
 		scanf("%s", nav1);
-			if(strcmp(nav1, "E")==0){
-			printf("\nYou feel truly refreshed. Perhaps a crash landing was just what you needed.");
-			printf("\n...shouldn't your team have checked in on you by now?\n");
-			sleep(5);
-			goto SUB1;
-			}
-			if(strcmp(nav1, "e")==0){
-			printf("\nYou feel truly refreshed. Perhaps a crash landing was just what you needed.");
-			printf("\n...shouldn't your team have checked in on you by now?\n");
-			sleep(5);
-			goto SUB1;
-			}
-			if(strcmp(nav1, "B")==0){
-			printf("\nAs beautiful as the clouds are, you have things to do.\n");
-			sleep(3);
-			goto SUB1;
-			}
-			if(strcmp(nav1, "b")==0){
-			printf("\nAs beautiful as the clouds are, you have things to do.\n");
-			sleep(3);
-			goto SUB1;
-			}
-			else {
-			goto SUB1;
+		if(strcmp(nav1, "E")==0){
+		printf("\nYou feel truly refreshed. Perhaps a crash landing was just what you needed.");
+		printf("\n...shouldn't your team have checked in on you by now?\n");
+		sleep(5);
+		goto SUB1;
+		}
+		if(strcmp(nav1, "e")==0){
+		printf("\nYou feel truly refreshed. Perhaps a crash landing was just what you needed.");
+		printf("\n...shouldn't your team have checked in on you by now?\n");
+		sleep(5);
+		goto SUB1;
+		}
+		if(strcmp(nav1, "B")==0){
+		printf("\nAs beautiful as the clouds are, you have things to do.\n");
+		sleep(3);
+		goto SUB1;
+		}
+		if(strcmp(nav1, "b")==0){
+		printf("\nAs beautiful as the clouds are, you have things to do.\n");
+		sleep(3);
+		goto SUB1;
+		}
+		else {
+		goto SUB1;
 	}}
 
-		if(strcmp(nav1, "B")==0){
+	if(strcmp(nav1, "B")==0){
 		printf("\nYou consider checking the mountain out...\nYou've got better things to do.\n");
 		sleep(2);
 		goto MAINMENU;
 	}
-		if(strcmp(nav1, "b")==0){
+	if(strcmp(nav1, "b")==0){
 		printf("\nYou consider checking the mountain out...\nYou've got better things to do.\n");
 		sleep(2);
 		goto MAINMENU;
@@ -229,56 +222,57 @@ SUB1:
 //BEGIN SUB-MENU TWO//	
 
 SUB2:
-		if(strcmp(opt1, "2")==0){
+	if(strcmp(opt1, "2")==0){
+	system("clear");
+	memset(nav1, '\0', 50);
+	printf("\nThe Plains v0.08\n\n");
+	printf("You sit down on the warm grass. You feel the sun shining...\nTimes like these are perfect to sit down and think.\n\n");
+	printf("\nRECENT EVENTS [1]");
+	printf("\nSURROUNDINGS [2]");
+	printf("\nABOUT YOURSELF [3]");
+	printf("\n[B]ACK \n\n");
+	printf("ACTION >> ");
+	scanf("%s", nav1);
+		
+	if(strcmp(nav1, "1")==0){
 		system("clear");
 		memset(nav1, '\0', 50);
 		printf("\nThe Plains v0.08\n\n");
-		printf("You sit down on the warm grass. You feel the sun shining...\nTimes like these are perfect to sit down and think.\n\n");
-		printf("\nRECENT EVENTS [1]");
-		printf("\nSURROUNDINGS [2]");
-		printf("\nABOUT YOURSELF [3]");
-		printf("\n[B]ACK \n\n");
-		printf("ACTION >> ");
+		printf("You were on a mission to a different planet, and something happened...\nyou can't exactly remember.\n\n");
+		printf("WHAT [E]LSE?\n");
+		printf("[B]ACK\n\n");
+		printf("\n\n\nACTION >> ");
 		scanf("%s", nav1);
-			if(strcmp(nav1, "1")==0){
-			system("clear");
-			memset(nav1, '\0', 50);
-			printf("\nThe Plains v0.08\n\n");
-			printf("You were on a mission to a different planet, and something happened...\nyou can't exactly remember.\n\n");
-			printf("WHAT [E]LSE?\n");
-			printf("[B]ACK\n\n");
-			printf("\n\n\nACTION >> ");
-			scanf("%s", nav1);
-			if(strcmp(nav1, "E")==0){
-			printf("\nBack at your home world, you are a successful astronaut, and spacetravel has been\nan interest of yours as long as you can remember.\n\n");
-			sleep(5);
-			printf("...you have a life to get back to. People that care about you.\n");
-			sleep(4);
-			goto SUB2;
-			}
-			if(strcmp(nav1, "e")==0){
-			printf("\nBack at your home world, you are a successful astronaut, and spacetravel has been\nan interest of yours as long as you can remember.\n\n");
-			sleep(5);
-			printf("...you have a life to get back to. People that care about you.\n");
-			sleep(4);
-			goto SUB2;
-			}
-			if(strcmp(nav1, "B")==0){
-			printf("\nIt's probably nothing to worry about.\n");
-			sleep(2);
-			goto SUB2;
-			}
-			if(strcmp(nav1, "b")==0){
-			printf("\nIt's probably nothing to worry about.\n");
-			sleep(2);
-			goto SUB2;
-			}
-			else {
-			goto SUB2;
-			}
+		if(strcmp(nav1, "E")==0){
+		printf("\nBack at your home world, you are a successful astronaut, and spacetravel has been\nan interest of yours as long as you can remember.\n\n");
+		sleep(5);
+		printf("...you have a life to get back to. People that care about you.\n");
+		sleep(4);
+		goto SUB2;
 		}
+		if(strcmp(nav1, "e")==0){
+		printf("\nBack at your home world, you are a successful astronaut, and spacetravel has been\nan interest of yours as long as you can remember.\n\n");
+		sleep(5);
+		printf("...you have a life to get back to. People that care about you.\n");
+		sleep(4);
+		goto SUB2;
+		}
+		if(strcmp(nav1, "B")==0){
+		printf("\nIt's probably nothing to worry about.\n");
+		sleep(2);
+		goto SUB2;
+		}
+		if(strcmp(nav1, "b")==0){
+		printf("\nIt's probably nothing to worry about.\n");
+		sleep(2);
+		goto SUB2;
+		}
+		else {
+		goto SUB2;
+		}
+	}
 
-		if(strcmp(nav1, "2")==0){
+	if(strcmp(nav1, "2")==0){
 		system("clear");
 		memset(nav1, '\0', 50);
 		printf("\nThe Plains v0.08\n\n");
@@ -287,32 +281,32 @@ SUB2:
 		printf("[B]ACK\n\n");
 		printf("\n\n\n\nACTION >> ");
 		scanf("%s", nav1);
-			if(strcmp(nav1, "E")==0){
-			printf("\nLooks pretty destroyed. You can see some foodstuffs and items you brought,\nbut the machine is toast.\n");
-			sleep(4);
-			printf("You decide to go back to where you landed.\n");
-			sleep(2);
-			goto MAINMENU;
-			}
-			if(strcmp(nav1, "e")==0){
-			printf("\nLooks pretty destroyed. You can see some foodstuffs and items you brought,\nbut the machine is toast.\n");
-			sleep(4);
-			printf("You decide to go back to where you landed.\n");
-			sleep(2);
-			goto MAINMENU;
-			}
-			if(strcmp(nav1, "B")==0){
-			goto SUB2;
-			}
-			if(strcmp(nav1, "B")==0){
-			goto SUB2;
-			}
-			else {
-			goto SUB2;
-			}
+		if(strcmp(nav1, "E")==0){
+		printf("\nLooks pretty destroyed. You can see some foodstuffs and items you brought,\nbut the machine is toast.\n");
+		sleep(4);
+		printf("You decide to go back to where you landed.\n");
+		sleep(2);
+		goto MAINMENU;
 		}
+		if(strcmp(nav1, "e")==0){
+		printf("\nLooks pretty destroyed. You can see some foodstuffs and items you brought,\nbut the machine is toast.\n");
+		sleep(4);
+		printf("You decide to go back to where you landed.\n");
+		sleep(2);
+		goto MAINMENU;
+		}
+		if(strcmp(nav1, "B")==0){
+		goto SUB2;
+		}
+		if(strcmp(nav1, "B")==0){
+		goto SUB2;
+		}
+		else {
+		goto SUB2;
+		}
+	}
 
-		if(strcmp(nav1, "3")==0){
+	if(strcmp(nav1, "3")==0){
 		system("clear");
 		memset(nav1, '\0', 50);
 		printf("\nThe Plains v0.08\n\n");
@@ -321,36 +315,36 @@ SUB2:
 		printf("[B]ACK\n\n");
 		printf("\n\n\n\nACTION >> ");
 		scanf("%s", nav1);
-			if(strcmp(nav1, "E")==0){
-			printf("\nYou're of small stature and tan shade. Your spacesuit is intricate and comfortable... Especially against this grass.\n");
-			sleep(4);
-			goto SUB2;
-			}
-			if(strcmp(nav1, "e")==0){
-			printf("\nYou're of small stature and tan shade. Your spacesuit is intricate and comfortable... Esepcially against this grass.\n");
-			sleep(4);
-			goto SUB2;
-			}
-			if(strcmp(nav1, "B")==0){
-			printf("\nWith newfound knowledge of yourself, you decide to head back.\n");
-			sleep(3);
-			goto SUB2;
-			}
-			if(strcmp(nav1, "b")==0){
-			printf("\nWith newfound knowledge of yourself, you decide to head back.\n");
-			sleep(3);
-			goto SUB2;
-			}
-			else {
-			goto SUB2;
-		}}
-	
+		if(strcmp(nav1, "E")==0){
+		printf("\nYou're of small stature and tan shade. Your spacesuit is intricate and comfortable... Especially against this grass.\n");
+		sleep(4);
+		goto SUB2;
+		}
+		if(strcmp(nav1, "e")==0){
+		printf("\nYou're of small stature and tan shade. Your spacesuit is intricate and comfortable... Esepcially against this grass.\n");
+		sleep(4);
+		goto SUB2;
+		}
 		if(strcmp(nav1, "B")==0){
+		printf("\nWith newfound knowledge of yourself, you decide to head back.\n");
+		sleep(3);
+		goto SUB2;
+		}
+		if(strcmp(nav1, "b")==0){
+		printf("\nWith newfound knowledge of yourself, you decide to head back.\n");
+		sleep(3);
+		goto SUB2;
+		}
+		else {
+		goto SUB2;
+	}}
+	
+	if(strcmp(nav1, "B")==0){
 		printf("\nSitting down on the grass is nice, but you've got work to do.\n");
 		sleep(2);
 		goto MAINMENU;
 	}
-		if(strcmp(nav1, "b")==0){
+	if(strcmp(nav1, "b")==0){
 		printf("\nSitting down on the grass is nice, but you've got work to do.\n");
 		sleep(2);
 		goto MAINMENU;
@@ -362,52 +356,53 @@ SUB2:
 //BEGIN SUB-MENU THREE//
 
 SUB3:
-		if(strcmp(opt1, "3")==0){
+	if(strcmp(opt1, "3")==0){
+	system("clear");
+	memset(nav1, '\0', 50);
+	printf("\nThe Plains v0.08\n\n");
+	printf("Stranded on this distant planet, you decide to use the tools at your disposal.\n\n");
+	printf("PHONE [1] \n");
+	printf("RADAR [2] \n");
+//	printf("SCANNER [3] \n");
+	printf("[B]ACK \n\n");
+	printf("\n\n\nACTION >> ");
+	scanf("%s", nav1);
+
+	if(strcmp(nav1, "1")==0){
 		system("clear");
 		memset(nav1, '\0', 50);
-		printf("\nThe Plains v0.08\n\n");
-		printf("Stranded on this distant planet, you decide to use the tools at your disposal.\n\n");
-		printf("PHONE [1] \n");
-		printf("RADAR [2] \n");
-	//	printf("SCANNER [3] \n");
-		printf("[B]ACK \n\n");
-		printf("\n\n\nACTION >> ");
+		printf("\nThe Plains v0.07\n\n");
+		printf("You pull out your phone. Unsurprisingly, the signal is rather weak.\n\n");
+		printf("T[E]XT A FRIEND\n");
+		printf("[B]ACK\n\n");
+		printf("\n\n\n\nACTION >> ");
 		scanf("%s", nav1);
-			if(strcmp(nav1, "1")==0){
-			system("clear");
-			memset(nav1, '\0', 50);
-			printf("\nThe Plains v0.07\n\n");
-			printf("You pull out your phone. Unsurprisingly, the signal is rather weak.\n\n");
-			printf("T[E]XT A FRIEND\n");
-			printf("[B]ACK\n\n");
-			printf("\n\n\n\nACTION >> ");
-			scanf("%s", nav1);
-			if(strcmp(nav1, "E")==0){
-			printf("\nYou try texting a friend from work. Your phone just shuts off...\n");
-			sleep(4);
-			goto SUB3;
-			}
-			if(strcmp(nav1, "e")==0){
-			printf("\nYou try texting a friend from work. Your phone just shuts off...\n");
-			sleep(4);
-			goto SUB3;
-			}
-			if(strcmp(nav1, "B")==0){
-			printf("\nWhat good's a phone when there's no connection?\n");
-			sleep(3);
-			goto SUB3;
-			}
-			if(strcmp(nav1, "b")==0){
-			printf("\nWhat good's a phone when there's no connection?\n");
-			sleep(3);
-			goto SUB3;
-			}
-			else {
-			goto SUB3;
-			}
+		if(strcmp(nav1, "E")==0){
+		printf("\nYou try texting a friend from work. Your phone just shuts off...\n");
+		sleep(4);
+		goto SUB3;
 		}
+		if(strcmp(nav1, "e")==0){
+		printf("\nYou try texting a friend from work. Your phone just shuts off...\n");
+		sleep(4);
+		goto SUB3;
+		}
+		if(strcmp(nav1, "B")==0){
+		printf("\nGuess there's no point in using a phone without a signal...\n");
+		sleep(3);
+		goto SUB3;
+		}
+		if(strcmp(nav1, "b")==0){
+		printf("\nGuess there's no point in using a phone without a signal...\n");
+		sleep(3);
+		goto SUB3;
+		}
+		else {
+		goto SUB3;
+		}
+	}
 
-		if(strcmp(nav1, "2")==0){
+	if(strcmp(nav1, "2")==0){
 		system("clear");
 		memset(nav1, '\0', 50);
 		printf("\nThe Plains v0.08\n\n");
@@ -416,30 +411,30 @@ SUB3:
 		printf("[B]ACK\n\n");
 		printf("\n\n\n\nACTION >> ");
 		scanf("%s", nav1);
-			if(strcmp(nav1, "E")==0){
-			printf("\nPushing the radar to its limits, you can see a signal coming from your galaxy.\nUnfortunately, your phone can't send signals that far.\n");
-			sleep(5);
-			goto SUB3;
-			}
-			if(strcmp(nav1, "e")==0){
-			printf("\nPushing the radar to its limits, you can see a signal coming from your galaxy.\nUnfortunately, your phone can't send signals that far.\n");
-			sleep(5);
-			goto SUB3;
-			}
-			if(strcmp(nav1, "B")==0){
-			sleep(1);
-			goto SUB3;
-			}
-			if(strcmp(nav1, "b")==0){
-			sleep(1);
-			goto SUB3;
-			}
-			else {
-			goto SUB3;
-			}
+		if(strcmp(nav1, "E")==0){
+		printf("\nPushing the radar to its limits, you can see a signal coming from your galaxy.\n");
+		sleep(3);
+		goto SUB3;
 		}
+		if(strcmp(nav1, "e")==0){
+		printf("\nPushing the radar to its limits, you can see a signal coming from your galaxy.\n");
+		sleep(3);
+		goto SUB3;
+		}
+		if(strcmp(nav1, "B")==0){
+		sleep(1);
+		goto SUB3;
+		}
+		if(strcmp(nav1, "b")==0){
+		sleep(1);
+		goto SUB3;
+		}
+		else {
+		goto SUB3;
+		}
+	}
 //TODO
-		if(strcmp(nav1, "3")==0){
+	if(strcmp(nav1, "3")==0){
 		system("clear");
 		memset(nav1, '\0', 50);
 		printf("\nThe Plains v0.08\n\n");
@@ -448,31 +443,31 @@ SUB3:
 		printf("[B]ACK\n\n");
 		printf("\n\n\n\nACTION >> ");
 		scanf("%s", nav1);
-			if(strcmp(nav1, "E")==0){
-			printf("Exiting... ");
-			goto SUB3;
-			}
-			if(strcmp(nav1, "e")==0){
-			printf("Exiting... ");
-			goto SUB3;
-			}
-			if(strcmp(nav1, "B")==0){
-			printf("Exiting... ");
-			goto SUB3;
-			}
-			if(strcmp(nav1, "b")==0){
-			printf("Exiting... ");
-			}
-			else {
-			goto SUB3;
-		}}
-
+		if(strcmp(nav1, "E")==0){
+		printf("Exiting... ");
+		goto SUB3;
+		}
+		if(strcmp(nav1, "e")==0){
+		printf("Exiting... ");
+		goto SUB3;
+		}
 		if(strcmp(nav1, "B")==0){
+		printf("Exiting... ");
+		goto SUB3;
+		}
+		if(strcmp(nav1, "b")==0){
+		printf("Exiting... ");
+		}
+		else {
+		goto SUB3;
+	}}
+
+	if(strcmp(nav1, "B")==0){
 		printf("\nHaving used your tools, you head back to where you landed.\n");
 		sleep(3);
 		goto MAINMENU;
 	}
-		if(strcmp(nav1, "b")==0){
+	if(strcmp(nav1, "b")==0){
 		printf("\nHaving used your tools, you head back to where you landed.\n");
 		sleep(3);
 		goto MAINMENU;
@@ -484,13 +479,13 @@ SUB3:
 //BEGIN HANDLER//
 
 HANDLER:
-		if(strcmp(opt1, "Q")==0){
+	if(strcmp(opt1, "Q")==0){
 		printf("\nExiting...\n");
 		sleep(1);
 		return 0;
 	}
 	
-		if(strcmp(opt1, "q")==0){
+	if(strcmp(opt1, "q")==0){
 		printf("\nExiting...\n");
 		sleep(1);
 		return 0;
