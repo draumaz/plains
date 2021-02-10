686a9a240fb91d56eaa105b2e2f1e17c3e10acc2
draumaz <77967471+draumaz@users.noreply.github.com>
2021-02-09 19:24:28 -0800

    Spacing

--- src/main_woe.c
+++ src/main_woe.c
@@ -16,7 +16,7 @@ int var4; //DEBUG FLAG//
 
 //INIT//
 
-system("cls");
+system("clear");
 var1 = 0;
 var2 = 0;
 var3 = 0;
@@ -67,6 +67,7 @@ MAINMENU:
 	sleep(2);
 	return 0;
 	}}
+
 	if(strcmp(opt1, "q")==0){
 	if(var2 == 1){
 	system("cls");
@@ -138,8 +139,7 @@ SUB1:
 			printf("GAME OVER\n\n");
 			sleep(5);
 			return 0;
-			}
-	
+			}	
 			if(strcmp(nav1, "e")==0){
 			printf("\nIn a moment of mind-blowing wit, you willingly let yourself die.\nNice going, genius.\n\n");
 			printf("GAME OVER\n\n");
@@ -156,7 +156,6 @@ SUB1:
 			sleep(2);
 			goto SUB1;
 			}
-	
 			else {
 			printf("\nDid you mean something else?\n");
 			sleep(1);
@@ -204,7 +203,7 @@ SUB1:
 				if(strcmp(nav1, "e")==0){
 				sleep(5);
 				var3 = 1;
-				printf("\nStanding there completely motionless, it gives you the feeling that eberything's going to be alright.\n");
+				printf("\nStanding there completely motionless, it gives you the feeling that everything's going to be alright.\n");
 				sleep(4);
 				goto SUB1;
 				}
@@ -222,8 +221,8 @@ SUB1:
 				printf("\nDid you mean something else?\n");
 				sleep(1);
 				goto SUB1;
-				}}
-
+				}
+			}
 			if(strcmp(nav1, "e")==0){
 			printf("\nYou continue standing there.\n\n");
 			sleep(10);
@@ -256,12 +255,13 @@ SUB1:
 				sleep(2);
 				goto SUB1;
 				}
-				else{
+				else {
 				printf("\nDid you mean something else?\n");
 				sleep(1);
 				goto SUB1;
-				}}	
+				}	
 			}
+		}
 	
 		if(strcmp(nav1, "3")==0){
 		system("cls");
@@ -301,19 +301,19 @@ SUB1:
 			printf("\nDid you mean something else?\n");
 			sleep(1);
 			goto SUB1;
-	}}
+			}
+		}
 
 		if(strcmp(nav1, "B")==0){
 		printf("\nYou consider checking the mountain out...\nYou've got better things to do.\n");
 		sleep(2);
 		goto MAINMENU;
-	}
+		}
 		if(strcmp(nav1, "b")==0){
 		printf("\nYou consider checking the mountain out...\nYou've got better things to do.\n");
 		sleep(2);
 		goto MAINMENU;
 		}
-
 		else {
 		printf("\nDid you mean something else?\n");
 		sleep(1);
@@ -340,8 +340,7 @@ SUB2:
 	printf("\nABOUT YOURSELF [3]");
 	printf("\n[B]ACK \n\n");
 	printf("ACTION >> ");
-	scanf("%s", nav1);
-	
+	scanf("%s", nav1);	
 		if(strcmp(nav1, "1")==0){
 		system("cls");
 		memset(nav1, '\0', 50);
@@ -460,19 +459,19 @@ SUB2:
 			printf("\nDid you mean something else?\n");
 			sleep(1);
 			goto SUB2;
-		}}
+			}
+		}
 	
 		if(strcmp(nav1, "B")==0){
 		printf("\nSitting down on the grass is nice, but you've got work to do.\n");
 		sleep(2);
 		goto MAINMENU;
-	}
+		}
 		if(strcmp(nav1, "b")==0){
 		printf("\nSitting down on the grass is nice, but you've got work to do.\n");
 		sleep(2);
 		goto MAINMENU;
 		}
-
 		else {
 		printf("\nDid you mean something else?\n");
 		sleep(1);
@@ -485,37 +484,37 @@ SUB2:
 //BEGIN TREE 3//
 
 SUB3:
-		if(strcmp(opt1, "3")==0){
-		var2 = 1;
+	if(strcmp(opt1, "3")==0){
+	var2 = 1;
+	system("cls");
+	memset(nav1, '\0', 50);
+	printf("\nThe Plains v0.11\n\n");
+	if(var4 == 1){
+	printf("OPT1 = %s, NAV1 = %s, VAR1 = %d\nVAR2 = %d, VAR3 = %d, VAR4 = %d\n\n", opt1, nav1, var1, var2, var3, var4);
+	}
+	printf("Stranded on this distant planet, you decide to use the tools at your disposal.\n\n");
+	printf("PHONE [1] \n");
+	printf("RADAR [2] \n");
+	printf("SCANNER [3] \n");
+	printf("[B]ACK \n\n");
+	printf("\n\n\nACTION >> ");
+	scanf("%s", nav1);
+		if(strcmp(nav1, "1")==0){
 		system("cls");
 		memset(nav1, '\0', 50);
+		var1 = 1;
 		printf("\nThe Plains v0.11\n\n");
 		if(var4 == 1){
 		printf("OPT1 = %s, NAV1 = %s, VAR1 = %d\nVAR2 = %d, VAR3 = %d, VAR4 = %d\n\n", opt1, nav1, var1, var2, var3, var4);
 		}
-		printf("Stranded on this distant planet, you decide to use the tools at your disposal.\n\n");
-		printf("PHONE [1] \n");
-		printf("RADAR [2] \n");
-		printf("SCANNER [3] \n");
-		printf("[B]ACK \n\n");
-		printf("\n\n\nACTION >> ");
+		printf("You pull out your phone. Unsurprisingly, the signal is rather weak.\n\n");
+		printf("T[E]XT A FRIEND\n");
+		printf("[B]ACK\n\n");
+		printf("\n\n\n\nACTION >> ");
 		scanf("%s", nav1);
-			if(strcmp(nav1, "1")==0){
-			system("cls");
-			memset(nav1, '\0', 50);
-			var1 = 1;
-			printf("\nThe Plains v0.11\n\n");
-			if(var4 == 1){
-			printf("OPT1 = %s, NAV1 = %s, VAR1 = %d\nVAR2 = %d, VAR3 = %d, VAR4 = %d\n\n", opt1, nav1, var1, var2, var3, var4);
-			}
-			printf("You pull out your phone. Unsurprisingly, the signal is rather weak.\n\n");
-			printf("T[E]XT A FRIEND\n");
-			printf("[B]ACK\n\n");
-			printf("\n\n\n\nACTION >> ");
-			scanf("%s", nav1);
 			if(strcmp(nav1, "E")==0){
 			printf("\nYou try texting a friend from work. Your phone just shuts off...\n");
-			sleep(4);
+			sleep(4);	
 			goto SUB3;
 			}
 			if(strcmp(nav1, "e")==0){
@@ -533,47 +532,47 @@ SUB3:
 			sleep(3);
 			goto SUB3;
 			}
+		else {
+		printf("\nDid you mean something else?\n");
+		sleep(1);
+		goto SUB3;
+		}
+	}
+
+		if(strcmp(nav1, "2")==0){
+		system("cls");
+		var1 = 1;
+		memset(nav1, '\0', 50);
+		printf("\nThe Plains v0.11\n\n");
+		if(var4 == 1){
+		printf("OPT1 = %s, NAV1 = %s, VAR1 = %d\nVAR2 = %d, VAR3 = %d, VAR4 = %d\n\n", opt1, nav1, var1, var2, var3, var4);
+		}
+		printf("Using your radar, you try to establish contact.\n\n");
+		printf("SCAN HARD[E]R\n");
+		printf("[B]ACK\n\n");
+		printf("\n\n\n\nACTION >> ");
+		scanf("%s", nav1);
+			if(strcmp(nav1, "E")==0){
+			printf("\nPushing the radar to its limits, you can see a signal coming from your galaxy.\nUnfortunately, your phone can't send signals that far.\n");
+			sleep(5);
+			goto SUB3;
+			}
+			if(strcmp(nav1, "e")==0){
+			printf("\nPushing the radar to its limits, you can see a signal coming from your galaxy.\nUnfortunately, your phone can't send signals that far.\n");
+			sleep(5);
+			goto SUB3;
+			}
+			if(strcmp(nav1, "B")==0){
+			goto SUB3;
+			}
+			if(strcmp(nav1, "b")==0){
+			goto SUB3;
+			}
 			else {
 			printf("\nDid you mean something else?\n");
 			sleep(1);
 			goto SUB3;
 			}
-		}
-
-			if(strcmp(nav1, "2")==0){
-			system("cls");
-			var1 = 1;
-			memset(nav1, '\0', 50);
-			printf("\nThe Plains v0.11\n\n");
-			if(var4 == 1){
-			printf("OPT1 = %s, NAV1 = %s, VAR1 = %d\nVAR2 = %d, VAR3 = %d, VAR4 = %d\n\n", opt1, nav1, var1, var2, var3, var4);
-			}
-			printf("Using your radar, you try to establish contact.\n\n");
-			printf("SCAN HARD[E]R\n");
-			printf("[B]ACK\n\n");
-			printf("\n\n\n\nACTION >> ");
-			scanf("%s", nav1);
-				if(strcmp(nav1, "E")==0){
-				printf("\nPushing the radar to its limits, you can see a signal coming from your galaxy.\nUnfortunately, your phone can't send signals that far.\n");
-				sleep(5);
-				goto SUB3;
-				}
-				if(strcmp(nav1, "e")==0){
-				printf("\nPushing the radar to its limits, you can see a signal coming from your galaxy.\nUnfortunately, your phone can't send signals that far.\n");
-				sleep(5);
-				goto SUB3;
-				}
-				if(strcmp(nav1, "B")==0){
-				goto SUB3;
-				}
-				if(strcmp(nav1, "b")==0){
-				goto SUB3;
-				}
-				else {
-				printf("\nDid you mean something else?\n");
-				sleep(1);
-				goto SUB3;
-				}
 		}
 
 			if(strcmp(nav1, "3")==0){
@@ -631,7 +630,8 @@ SUB3:
 				printf("\nDid you mean something else?\n");
 				sleep(1);
 				goto SUB3;
-		}}
+				}
+			}
 
 		if(strcmp(nav1, "B")==0){
 		if(var1 == 1){
@@ -680,27 +680,28 @@ SUB4:
 	memset(nav1, '\0', 50);
 	scanf("%s", nav1);
 		if(strcmp(nav1, "1")==0){
-			memset(nav1, '\0', 50);
-			printf("\nA spaceship that looks much like yours lands down on the surface of this strange world.\nGrass gets blown everywhere, and as it lands, you see your friends walk out.\n");
-			sleep(4);
-			printf("\nTO BE CONTINUED...\n");
-			sleep(3);
-			system("cls");
-			printf("\n==TRUE ENDING==\n");
-			sleep(2);
-			system("cls");
-			printf("\n==THANK YOU==\n==FOR PLAYING!==\n");
-			sleep(2);
-			return 0;
+		memset(nav1, '\0', 50);
+		printf("\nA spaceship that looks much like yours lands down on the surface of this strange world.\nGrass gets blown everywhere, and as it lands, you see your friends walk out.\n");
+		sleep(4);
+		printf("\nTO BE CONTINUED...\n");
+		sleep(3);
+		system("cls");
+		printf("\n==TRUE ENDING==\n");
+		sleep(2);
+		system("cls");
+		printf("\n==THANK YOU==\n==FOR PLAYING!==\n");
+		sleep(2);
+		return 0;
 		}
 		else {
 		goto SUB4;
 		}
 	}
+	
 	if(var3 == 0){
 	printf("\nDid you mean something else?\n");
 	sleep(1);
-			goto MAINMENU;
+	goto MAINMENU;
 	}}
 
 
