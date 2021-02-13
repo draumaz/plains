5b412f664814da1b619c49cd3d23a6986bbd333f
draumaz <77967471+draumaz@users.noreply.github.com>
2021-02-12 20:29:48 -0800

    Update toolsel3.py

--- src/toolsel3.py
+++ src/toolsel3.py
@@ -21,6 +21,7 @@ def toolSel3():
         data = 1
         with open(fname, 'w') as f:
             f.write('var2 = {}'.format(data))
+            f.close()
         time.sleep(4)
         tool.tool()
     if toolSelect3 == 2:
