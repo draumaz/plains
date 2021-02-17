34221dba7263fee94da12644366477cd455c1c0c
draumaz <77967471+draumaz@users.noreply.github.com>
2021-02-17 00:30:19 -0800

    Update friendflagger1.py

--- src/friendflagger1.py
+++ src/friendflagger1.py
@@ -3,7 +3,7 @@ import time
 import configparser
 import friend1
 
-def friendFlagger1():
+def friendFlagger1(): #Rude Flag
     config = configparser.ConfigParser()
     config['friendflag'] = {'var4': '2'}
     with open('config4.ini', 'w') as configfile:
