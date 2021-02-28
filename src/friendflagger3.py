df95b59107638c8fc3067f5e8e62cf9050148bda
draumaz <77967471+draumaz@users.noreply.github.com>
2021-02-28 09:32:13 -0800

    Delete friendflagger3.py

--- src/friendflagger3.py
+++
@@ -1,9 +0,0 @@
-import configparser
-import saan
-
-def friendFlagger3(): #Neutral Flag
-    config = configparser.ConfigParser()
-    config['friendflag'] = {'var4': '1'}
-    with open('save/config4.ini', 'w') as configfile:
-        config.write(configfile)
-    saan()
