210b58e1e8fc61a2057256504efb456338b36bfe
draumaz <77967471+draumaz@users.noreply.github.com>
2021-02-28 04:03:40 -0800

    Added file verification-inator

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
 import time
 import configparser
+from configparser import NoOptionError
+from configparser import NoSectionError
 import mm1
 import mm2
 import mainalt
@@ -8,6 +10,54 @@ import mainext
 import reset
 import settings
 import textwrap
+
+def fileTest():
+    while True:
+        try:
+            config = configparser.ConfigParser() #Verify config files are where they should be
+            config.read('save/config.ini')
+            var1 = config.getint('ch1endflag', 'var1')
+            config = configparser.ConfigParser()
+            config.read('save/config2.ini')
+            var2 = config.getint('chaptflagger', 'var2')
+            config = configparser.ConfigParser()
+            config.read('save/config3.ini')
+            var3 = config.getint('toolflag', 'var3')
+            config = configparser.ConfigParser()
+            config.read('save/config4.ini')
+            var4 = config.getint('friendflag', 'var4')
+            config = configparser.ConfigParser()
+            config.read('save/config5.ini')
+            var5 = config.getint('reset', 'var5')
+            config = configparser.ConfigParser()
+            config.read('save/config6.ini')
+            var6 = config.getint('lizard', 'var6')
+            config = configparser.ConfigParser()
+            config.read('save/config7.ini')
+            var7 = config.getint('lizardext', 'var7')
+            config = configparser.ConfigParser()
+            config.read('save/config8.ini')
+            var8 = config.getint('okay', 'var8')
+            config = configparser.ConfigParser()
+            config.read('save/config9.ini')
+            var9 = config.getint('badend', 'var9')
+            config = configparser.ConfigParser()
+            config.read('save/config10.ini')
+            var10 = config.getint('badendext', 'var10')
+            config = configparser.ConfigParser()
+            config.read('save/config11.ini')
+            var11 = config.getint('splashskip', 'var11')
+            config = configparser.ConfigParser()
+            config.read('save/config12.ini')
+            var12 = config.getint('lizarddx', 'var12')
+            config = configparser.ConfigParser()
+            config.read('save/config13.ini')
+            var13 = config.getint('splashskip2', 'var13')
+            preSplash()
+        except (NoOptionError, NoSectionError):
+            print('')
+            print(textwrap.fill("The game was not able to detect critical files. Please double check that the executable is in the same folder as the save directory, and restart the game.", 75))
+            quit()
 
 def preSplash():
     config = configparser.ConfigParser()
@@ -104,11 +154,6 @@ def splashScreen2():
             try:
                 bsSel1 = int(input('\nACTION >> '))
 
-                while bsSel1 < 1 or bsSel1 > 3:
-                    print('\nDid you mean something else?')
-                    time.sleep(0.5)
-                    splashScreen2()
-
                 if bsSel1 == 1: #Chapter Save Director (expandable)
                     print('\nScanning save...')
                     time.sleep(0.5)
@@ -129,4 +174,4 @@ def splashScreen2():
                 time.sleep(0.5)
                 splashScreen2()
 
-preSplash()
+fileTest() #Called when executed
