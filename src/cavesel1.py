7e30dc6345d4d2b89cc91eb4f075ccb2c9153603
draumaz <77967471+draumaz@users.noreply.github.com>
2021-03-02 11:08:56 -0800

    Add files via upload

--- src/cavesel1.py
+++ src/cavesel1.py
@@ -6,11 +6,11 @@ import cave
 
 def cs1Knife():
     print('\nYou open the box and find a knife. You put it in your pocket.')
-    time.sleep(3)
     config = configparser.ConfigParser()
     config['blade'] = {'var15': '1'}
     with open('save/config15.ini', 'w') as configfile:
         config.write(configfile)
+    time.sleep(3)
     caveSel1()
 
 def caveSel1():
@@ -21,10 +21,9 @@ def caveSel1():
     print('\nThe Plains v0.20\n')
     if var15 == 0:
         print("You continue deeper down the cave. There's a small box sitting near the wall.\n")
+        print('OPEN [1]')
     if var15 == 1:
         print("Just a dingy old cave.\n")
-    if var15 == 0:
-        print('OPEN [1]')
     print('BACK [2]')
 
     while True:
