3b8dd4cad7c7babeb1709503ba9c3fe11afad9df
draumaz <77967471+draumaz@users.noreply.github.com>
2021-03-18 11:13:32 -0700

    Derp

--- src/hill.py
+++ src/hill.py
@@ -136,7 +136,7 @@ def hillSel1Ext():
             hillSel1ExtSel = int(input('\nACTION >> '))
 
             if hillSel1ExtSel == 1:
-                if var16 == 1:
+                if var16 == 2 and var15 == 1:
                     print('\nI genuinely do not have the heart to program a scenario for this.')
                     time.sleep(2)
                     print('Sorry!')
