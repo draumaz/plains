91d9bea5d63ca1f3aef7f4ee1c1481b0f9338ecf
draumaz <77967471+draumaz@users.noreply.github.com>
2022-02-17 19:21:18 -0800

    Removed Windows STOP_CMD

--- src/base/screen_manip.c
+++ src/base/screen_manip.c
@@ -1,12 +1,6 @@
 #include <curses.h>
 #include <stdlib.h>
 
-#ifdef _WIN32
-	#define STOP_CMD "pause"
-#else
-	#define STOP_CMD "stty sane"
-#endif
-
 void screen_up() {
 	initscr();
 	noecho();
@@ -20,5 +14,5 @@ void screen_down() {
 	keypad(stdscr, false);
 	endwin();
 	curs_set(1);
-	system(STOP_CMD);
+	system("stty sane");
 }
