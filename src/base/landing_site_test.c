979619fdbf1b100c24202a9fed2f89db7423363d
draumaz <77967471+draumaz@users.noreply.github.com>
2022-02-17 19:29:03 -0800

    Abstracted header into func

--- src/base/landing_site_test.c
+++ src/base/landing_site_test.c
@@ -5,30 +5,29 @@
 
 #include "./../header/screen_manip.h"
 
+void crashland_head() {
+	move(1, 0);
+	printw("The Plains v0.26");
+	move(3, 0);
+	printw("You are Liam. An astronaut by trade, you took a bad turn on the");
+	move(4, 0);
+	printw("Space Belt, and crash-landed on this strange, alien planet.");
+	move(5, 0);
+	printw("You awaken, lain on a vast field of grass.");
+	move(0, 0);
+}
+
 void land_test() {
 	screen_up();
 	int game_loop = 0;
 	int game_loop2 = 0;
 	int active_x = 0;
-	int active_y = 0;
-	active_y = 1;
+	int active_y = 7;
 	char* sel_txt[4] = {"[HILL ]", "[CAVE ]", "[TOOLS]", "[QUIT ]"};
-	move(active_y, 0);
-	printw("The Plains v0.26");
-	active_y = 3;
-	move(active_y, 0);
-	printw("You are Liam. An astronaut by trade, you took a bad turn on the");
-	active_y = 4;
-	move(active_y, 0);
-	printw("Space Belt, and crash-landed on a strange planet.");
-	active_y = 5;
-	move(active_y, 0);
-	printw("You awaken, laying in a field of grass.");
-	active_y = 7;
-	move(active_y, 0);
+	crashland_head();
+	move(7, 0);
 	printw("%s", sel_txt[0]);
-	active_y = 8;
-	move(active_y, 0);
+	move(8, 0);
 	printw("%s", sel_txt[1]);
 	active_y = 9;
 	move(active_y, 0);
@@ -38,8 +37,6 @@ void land_test() {
 	printw("%s", sel_txt[3]);
 	active_y = 7;
 	active_x = 8;
-	move(active_y, active_x);
-	refresh();
 	while (game_loop == 0) {
 		while (game_loop2 == 0) {
 			move(active_y, active_x);
--- src/header/landing_site_test.h
+++ src/header/landing_site_test.h
@@ -1,1 +1,2 @@
+void crashland_head();
 void land_test();
