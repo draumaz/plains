2fbeba17627e7b577d54bb7116e8a68956f4aece
draumaz <77967471+draumaz@users.noreply.github.com>
2022-02-21 20:49:16 -0800

    removed ghoulish indentation

--- src/base/c1_hill_subs.c
+++ src/base/c1_hill_subs.c
@@ -12,45 +12,45 @@
 #include "../header/c1_txt.h"
 
 struct c1h_subs {
-    int head_loop;
-    int body_loop;
-    int active_x;
-    int active_y;
-    int disp_inc;
+	int head_loop;
+	int body_loop;
+	int active_x;
+	int active_y;
+	int disp_inc;
 };
 
 void hill_subs_river() {
-    struct c1h_subs c;
-    c.head_loop = 0;
-    c.body_loop = 0;
-    c.disp_inc = 0;
-    c.active_y = HILL_SUBS_RIVER_OPTS_MIN;
-    c.active_x = 8;
-    int bottle_cont = save_reader()[3];
-    char* sel_txt[3];
-    sel_txt[2] = "[BACK ]";
-    sel_txt[0] = "[RELAX]";
-    char* head_txt = "A river gently streams through this strange planet.";
-    switch (bottle_cont) {
-        case 0: // don't have
-        case 2:
-            sel_txt[1] = "[CHILL]";
-            break;
-        case 1: // empty
-            sel_txt[1] = "[FILL ]";
-            break;
-        case 3: // water
-            sel_txt[1] = "[EMPTY]";
-            break;
-    }
-    for (int i = HILL_SUBS_RIVER_OPTS_MIN; i < HILL_SUBS_RIVER_OPTS_MAX+1; i++) {
-        move(i, 0);
-        printw("%s", sel_txt[c.disp_inc]);
-        c.disp_inc++;
-    }
-    move(3, 0);
-    printw("%s", head_txt);
-    while (c.head_loop == 0) {
+	struct c1h_subs c;
+	c.head_loop = 0;
+	c.body_loop = 0;
+	c.disp_inc = 0;
+	c.active_y = HILL_SUBS_RIVER_OPTS_MIN;
+	c.active_x = 8;
+	int bottle_cont = save_reader()[3];
+	char* sel_txt[3];
+	sel_txt[2] = "[BACK ]";
+	sel_txt[0] = "[RELAX]";
+	char* head_txt = "A river gently streams through this strange planet.";
+	switch (bottle_cont) {
+		case 0: // don't have
+		case 2:
+			sel_txt[1] = "[CHILL]";
+			break;
+		case 1: // empty
+			sel_txt[1] = "[FILL ]";
+			break;
+		case 3: // water
+			sel_txt[1] = "[EMPTY]";
+			break;
+	}
+	for (int i = HILL_SUBS_RIVER_OPTS_MIN; i < HILL_SUBS_RIVER_OPTS_MAX+1; i++) {
+		move(i, 0);
+		printw("%s", sel_txt[c.disp_inc]);
+		c.disp_inc++;
+	}
+	move(3, 0);
+	printw("%s", head_txt);
+	while (c.head_loop == 0) {
 		while (c.body_loop == 0) {
 			move(c.active_y, c.active_x);
 			printw("<");
@@ -86,35 +86,35 @@ void hill_subs_river() {
 					break;
 			}
 		}
-        switch (c.active_y) {
-            case 6:
-                move(HILL_SUBS_RIVER_OPTS_MAX+2, 0);
-                if (bottle_cont == 0) {
-                    printw("...isn't this the same thing as relaxing?");
-                } else if (bottle_cont == 1) {
-                    save_writer(3, 3);
-                    printw("You collect some water into your bottle.");
-                } else if (bottle_cont == 3) {
-                    save_writer(3, 1);
-                    printw("You empty your bottle out into the river.");
-                }
-                refresh();
-                scr_sleep(500);
-                move(HILL_SUBS_RIVER_OPTS_MAX+2, 0);
-                printw("\n");
-                the_wiper(TIPPY_HEAD_MIN, TIPPY_HEAD_MAX);
-                tippy_head();
-                the_wiper(HILL_SUBS_RIVER_OPTS_MIN, HILL_SUBS_RIVER_OPTS_MAX);
-                hill_subs_river();
-                break;
-            case 7:
-                the_wiper(3, 4);
-                the_wiper(HILL_SUBS_RIVER_OPTS_MIN, HILL_SUBS_RIVER_OPTS_MAX+1);
-                hill();
-                break;
-            default:
-                c.body_loop = 0;
-                break;
-        }
-    }
+		switch (c.active_y) {
+			case 6:
+				move(HILL_SUBS_RIVER_OPTS_MAX+2, 0);
+				if (bottle_cont == 0) {
+					printw("...isn't this the same thing as relaxing?");
+				} else if (bottle_cont == 1) {
+					save_writer(3, 3);
+					printw("You collect some water into your bottle.");
+				} else if (bottle_cont == 3) {
+					save_writer(3, 1);
+					printw("You empty your bottle out into the river.");
+				}
+				refresh();
+				scr_sleep(500);
+				move(HILL_SUBS_RIVER_OPTS_MAX+2, 0);
+				printw("\n");
+				the_wiper(TIPPY_HEAD_MIN, TIPPY_HEAD_MAX);
+				tippy_head();
+				the_wiper(HILL_SUBS_RIVER_OPTS_MIN, HILL_SUBS_RIVER_OPTS_MAX);
+				hill_subs_river();
+				break;
+			case 7:
+				the_wiper(3, 4);
+				the_wiper(HILL_SUBS_RIVER_OPTS_MIN, HILL_SUBS_RIVER_OPTS_MAX+1);
+				hill();
+				break;
+			default:
+				c.body_loop = 0;
+				break;
+		}
+	}
 }
