41dd7a79accd06cd0371f44359e3bb1557eb1c1d
draumaz <draumaz@goatopossum.com>
2022-07-26 04:15:19 +0000

    continued work

---
+++ src/b_display.rs
@@ -0,0 +1,60 @@
+extern crate pancurses;
+
+use pancurses::{Input, resize_term, echo, noecho, curs_set};
+
+pub fn universal_tabler(
+	win: &pancurses::Window,
+	first: i32,
+	last: i32,
+	x:     i32,
+	mut y: i32) 
+-> i32 { 
+	loop {
+		win.mv(y, x);
+		win.printw("<");
+		match win.getch() {
+			Some(Input::KeyUp) | 
+			Some(Input::Character('w')) |
+			Some(Input::Character('i')) => {
+				win.mv(y, x);
+				win.printw("\n");
+				if y == first { y = last } else { y -= 1 }
+			},
+			Some(Input::KeyDown) |
+			Some(Input::Character('s')) |
+			Some(Input::Character('k')) => {
+				win.mv(y, x);
+				win.printw("\n");
+				if y == last { y = first } else { y += 1 }
+			},
+			Some(Input::Character('\n')) => { 
+				win.mv(y, x);
+				win.printw("\n");
+				return y },
+			Some(_) => (),
+			None => ()
+		}
+	} 
+}
+
+pub fn screen_smash(
+	win: &pancurses::Window, 
+	min: i32, 
+	max: i32) 
+{
+	for i in min..max {
+		win.mv(i, 0);
+		win.printw("\n");
+		win.refresh();
+	}
+}
+pub fn screen_up(s: &pancurses::Window) {
+	s.keypad(true);
+	resize_term(25, 80);
+	curs_set(0);
+	noecho();
+}
+pub fn screen_down() {
+	curs_set(1);
+	echo();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate pancurses;
+
+mod b_display;
+
+use b_display::{universal_tabler, screen_up, screen_down};
+use pancurses::{initscr, endwin};
+
+fn splash(win: &pancurses::Window) {
+    win.mv(1, 0);
+    win.printw("==THE PLAINS===============\n==DRAUMAZ, 2021-22=========\n==WRITTEN IN RUST!=========");
+    win.mv(5, 0);
+    win.printw("[PLAY   ]\n[RESET  ]\n[LICENSE]\n[QUIT   ]");
+    loop { match universal_tabler(&win, 5, 8, 11, 5) {
+        5|6|7 => {continue}
+        8|_ => {break}
+    } }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let screen = initscr();
+    screen_up(&screen);
+    splash(&screen);
+    screen_down();
+    endwin();
 }
