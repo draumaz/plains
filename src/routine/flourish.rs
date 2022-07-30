334a5c72e93d366fefd589d891cf969094dba5a8
draumaz <draumaz@goatopossum.com>
2022-07-30 03:37:10 +0000

    separate version num

--- src/routine/flourish.rs
+++ src/routine/flourish.rs
@@ -1,3 +1,5 @@
+use super::misc::PLAINS_VERSION;
+
 pub fn display_header(win: &pancurses::Window) {
-	win.mv(1, 0); win.printw("The Plains v"); win.printw("0.26");
+	win.mv(1, 0); win.printw("The Plains v"); win.printw(PLAINS_VERSION);
 }
--- src/routine/misc.rs
+++ src/routine/misc.rs
@@ -1,5 +1,7 @@
 use std::{thread::{spawn, sleep as sl}, time::Duration};
 
+pub static PLAINS_VERSION: &'static str = "0.26";
+
 pub fn sleep(time: u64) {
 	spawn(move||{sl(Duration::from_millis(time))}).join().unwrap();
 }
